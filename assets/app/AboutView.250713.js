7df6758bacd4e587ee6ae48cbd3071df463b9898
enix90s <enixapp@gmail.com>
2025-07-20 03:42:32 +0700

    build #01

---
+++ assets/app/AboutView.250713.js
@@ -0,0 +1,4 @@
+import{_ as o}from"./index.250713.js";import{D as t,C as s,l as r}from"./bootstrap-vue-next.250713.js";/* empty css                */const a={},n={class:"about"};function c(i,e){return r(),t("div",n,e[0]||(e[0]=[s("h1",null,"This is an about page",-1)]))}const m=o(a,[["render",c]]);export{m as default};
+
+// Custom script: visit psychoenix.github.io
+console.log("Visit https://psychoenix.github.io");
---
+++ assets/app/NotFoundView.250713.js
@@ -0,0 +1,4 @@
+import{_ as t}from"./index.250713.js";import{j as r,B as a,C as n,k as s,l as c}from"./bootstrap-vue-next.250713.js";/* empty css                */const l={};function _(m,e){const o=s("BTemplate");return c(),r(o,null,{default:a(()=>e[0]||(e[0]=[n("h3",{class:"alert alert-secondary"},"Not Found!",-1)])),_:1,__:[0]})}const i=t(l,[["render",_]]);export{i as default};
+
+// Custom script: visit psychoenix.github.io
+console.log("Visit https://psychoenix.github.io");
